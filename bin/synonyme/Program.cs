2be525824add92dc8e62746befd4a32173e3fefa
Bernhard Pichler <bernhard.robert.pichler@gmail.com>
2012-09-22 14:15:40 +0200

    finished first pages, more to come

---
+++ bin/synonyme/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.IO;
+
+namespace SynonymeConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<String> synonymIDs = new HashSet<string>();
+
+            Dictionary<string, string> prettifies = new Dictionary<string, string>();
+            prettifies["javascript"] = "lang-js";
+            prettifies["dart"] = "lang-java";
+            prettifies["java"] = "lang-java";
+            prettifies["csharp"] = "lang-java";
+            prettifies["as3"] = "lang-js";
+
+            Dictionary<string, string> languageNames = new Dictionary<string, string>();
+            languageNames["javascript"] = "JavaScript";
+            languageNames["dart"] = "Dart";
+            languageNames["java"] = "Java";
+            languageNames["csharp"] = "C#";
+            languageNames["as3"] = "ActionScript";
+
+            string path = @".\\data";
+            string language1 = "as3";
+            string language2 = "dart";
+
+            string htmlFile = string.Format("{0}\\synonyms.html", path, language1);
+
+            //----------------------------------------------------------------------------------------
+
+            Directory.CreateDirectory(Path.GetDirectoryName(htmlFile));
+            File.Delete(htmlFile);
+
+            //----------------------------------------------------------------------------------------
+
+            XDocument doc = XDocument.Load(path + "\\synonyms.xml");
+
+            foreach (var theme in doc.Root.Elements("theme"))
+            {
+                var themeId = theme.Attribute("id").Value;
+                var themeTitle = theme.Element("title").Value;
+
+                StringBuilder synonymHtml = new StringBuilder();
+
+                List<string> themeSynonyms = new List<string>();
+
+                foreach (var synonym in theme.Elements("synonym"))
+                {
+                    var synonymID = (synonym.Attribute("id") != null) ? synonym.Attribute("id").Value : null;
+                    var synonymTitle = (synonym.Element("title") != null) ? synonym.Element("title").Value : null;
+
+                    if (synonymID == null)
+                        throw new Exception("missing synonymeID");
+
+                    if (synonymIDs.Add(synonymID) == false)
+                        throw new Exception("duplicate synonymeID");
+
+                    synonymID = synonymID.Replace("syn-", "snippet-");
+
+                    var code1 = synonym.Elements("code").Where((e) => e.Attribute("language").Value == language1).FirstOrDefault().Value;
+                    var code2 = synonym.Elements("code").Where((e) => e.Attribute("language").Value == language2).FirstOrDefault().Value;
+
+                    code1 = code1.Replace("\t", "  ");
+                    code2 = code2.Replace("\t", "  ");
+
+                    synonymHtml.AppendFormat("  <section class=\"section-synonym\">\n");
+                    synonymHtml.AppendFormat("    <div class=\"row\">\n");
+                    synonymHtml.AppendFormat("      <div class=\"span12\"><h3 class=\"section\">{0}</h3></div>\n", EscapeHtml(synonymTitle));
+                    synonymHtml.AppendFormat("    </div>\n");
+                    synonymHtml.AppendFormat("    <div class=\"row\">\n");
+                    synonymHtml.AppendFormat("      <div class=\"span6\"><pre class=\"prettyprint {0}\">{1}</pre></div>\n", prettifies[language1], EscapeHtml(code1));
+                    synonymHtml.AppendFormat("      <div class=\"span6\"><pre class=\"prettyprint {0}\">{1}</pre></div>\n", prettifies[language2], EscapeHtml(code2));
+                    synonymHtml.AppendFormat("    </div>\n");
+                    synonymHtml.AppendFormat("  </section>\n");
+                }
+
+                //-------------------------------------------------
+
+                StringBuilder themeHtml = new StringBuilder();
+                themeHtml.AppendFormat("<section class=\"section-theme\" id=\"{0}\">\n", themeId);
+                themeHtml.AppendFormat("  <div class=\"row\"><div class=\"span12\"><h2>{0}</h2></div></div>\n", EscapeHtml(themeTitle));
+                themeHtml.Append(synonymHtml.ToString());
+                themeHtml.AppendFormat("</section>\n");
+
+                File.AppendAllText(htmlFile, themeHtml.ToString());
+
+            }
+        }
+
+        public static string EscapeHtml(string html)
+        {
+            return html
+                .Replace("&", "&amp;")
+                .Replace("<", "&lt;")
+                .Replace(">", "&gt;")
+                .Replace("\"", "&quot;")
+                .Replace("'", "&apos;");
+        }
+    }
+}
---
+++ web/demos/masking/masking.dart
@@ -0,0 +1,89 @@
+#import('dart:math');
+#import('dart:html', prefix:'html');
+#import('../../../packages/dartflash/dartflash.dart');
+
+Resource resource;
+
+void main()
+{
+  //------------------------------------------------------------------
+  // Initialize the Display List
+  //------------------------------------------------------------------
+
+  Stage stage = new Stage('myStage', html.document.query('#stage'));
+
+  RenderLoop renderLoop = new RenderLoop();
+  renderLoop.addStage(stage);
+
+  //------------------------------------------------------------------
+  // prepare different Masks for later use
+  //------------------------------------------------------------------
+
+  List<Point> starPath = new List<Point>();
+
+  for(int i = 0; i < 6; i++) {
+    num a1 = PI * (i * 60.0) / 180.0;
+    num a2 = PI * (i * 60.0 + 30.0) / 180.0;
+    starPath.add(new Point(470.0 + 200.0 * cos(a1), 250.0 + 200.0 * sin(a1)));
+    starPath.add(new Point(470.0 + 100.0 * cos(a2), 250.0 + 100.0 * sin(a2)));
+  }
+
+  Mask rectangleMask = new Mask.rectangle(100.0, 100.0, 740.0, 300.0);
+  Mask circleMask = new Mask.circle(470.0, 250.0, 200.0);
+  Mask customMask = new Mask.custom(starPath);
+
+  //------------------------------------------------------------------
+  // Use the Resource class to load some Bitmaps
+  //------------------------------------------------------------------
+
+  resource = new Resource();
+  resource.addImage('flower1', '../common/images/Flower1.png');
+  resource.addImage('flower2', '../common/images/Flower2.png');
+  resource.addImage('flower3', '../common/images/Flower3.png');
+
+  resource.load().then((result)
+  {
+    //------------------------------------------------------------------
+    // Draw a nice looking animation
+    //------------------------------------------------------------------
+
+    Sprite animation = new Sprite();
+    Random random = new Random();
+
+    for(int i = 0; i < 150; i++) {
+      int f = 1 + random.nextInt(3);
+      BitmapData bitmapData = resource.getBitmapData('flower$f');
+      Bitmap bitmap = new Bitmap(bitmapData);
+      bitmap.pivotX = 64;
+      bitmap.pivotY = 64;
+      bitmap.x = 64.0 + random.nextDouble() * (940 - 128);
+      bitmap.y = 64.0 + random.nextDouble() * (500 - 128);
+      animation.addChild(bitmap);
+
+      Tween tween = new Tween(bitmap, 600, Transitions.linear);
+      tween.animate('rotation', PI * 60.0);
+      Juggler.instance.add(tween);
+    }
+
+    animation.pivotX = 470;
+    animation.pivotY = 250;
+    animation.x = 470;
+    animation.y = 250;
+    stage.addChild(animation);
+
+    //------------------------------------------------------------------
+    // add html-button event listeners
+    //------------------------------------------------------------------
+
+    html.query('#mask-none').on.click.add((e) => animation.mask = null);
+    html.query('#mask-rectangle').on.click.add((e) => animation.mask = rectangleMask);
+    html.query('#mask-circle').on.click.add((e) => animation.mask = circleMask);
+    html.query('#mask-custom').on.click.add((e) => animation.mask = customMask);
+    html.query('#mask-spin').on.click.add((e) {
+      Tween rotate = new Tween(animation, 2.0, Transitions.easeInOutBack);
+      rotate.animate('rotation', PI * 4.0);
+      rotate.onComplete = () => animation.rotation = 0.0;
+      Juggler.instance.add(rotate);
+    });
+  });
+}

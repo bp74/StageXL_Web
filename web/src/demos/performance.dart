9381dc5908582ee5f0073ed0444eb2a7e8962af4
Bernhard <bernhard.robert.pichler@gmail.com>
2013-05-31 20:28:23 +0200

    added Flash version of performance demo

--- web/src/demos/performance.dart
+++ web/src/demos/performance.dart
@@ -1,7 +1,7 @@
 part of stagexl_demos;
 
 class FlyingFlag extends Bitmap implements Animatable {
-  
+
     num vx, vy;
 
     FlyingFlag(BitmapData bitmapData, this.vx, this.vy):super(bitmapData) {
@@ -22,70 +22,68 @@ class FlyingFlag extends Bitmap implements Animatable {
 //-----------------------------------------------------------------------------------
 
 class PerformanceDemo extends DisplayObjectContainer {
-  
+
   PerformanceDemo() {
-    
-    // let's start with 250 flags
-    _addFlags(250);
+
+    // let's start with 500 flags
+    _addFlags(500);
 
     // add html-button event listeners
-    html.query('#minus10').onClick.listen((e) => _removeFlags(10));
-    html.query('#minus50').onClick.listen((e) => _removeFlags(50));
-    html.query('#plus50').onClick.listen((e) => _addFlags(50));
-    html.query('#plus10').onClick.listen((e) => _addFlags(10));
+    html.query('#minus100').onClick.listen((e) => _removeFlags(100));
+    html.query('#plus100').onClick.listen((e) => _addFlags(100));
 
     // add event listener for EnterFrame (fps meter)
-    this.onEnterFrame.listen(_onEnterFrame);            
+    this.onEnterFrame.listen(_onEnterFrame);
   }
-  
+
   //---------------------------------------------------------------------------------
 
   num _fpsAverage = null;
-  
+
   _onEnterFrame(EnterFrameEvent e) {
-    
+
     if (_fpsAverage == null) {
       _fpsAverage = 1.00 / e.passedTime;
     } else {
       _fpsAverage = 0.05 / e.passedTime + 0.95 * _fpsAverage;
     }
-  
+
     html.query('#fpsMeter').innerHtml = 'fps: ${_fpsAverage.round()}';
   }
-  
+
   //---------------------------------------------------------------------------------
-  
+
   _addFlags(int amount) {
-    
+
     var random = new Random();
     var textureAtlas = resourceManager.getTextureAtlas('flags');
     var flagNames = textureAtlas.frameNames;
-    
+
     while(--amount >= 0) {
       var flagName = flagNames[random.nextInt(flagNames.length)];
       var flagBitmapData = textureAtlas.getBitmapData(flagName);
-      
+
       var flyingFlag = new FlyingFlag(flagBitmapData, random.nextInt(200) - 100, random.nextInt(200) - 100);
       flyingFlag.x = 30 + random.nextInt(940 - 60);
       flyingFlag.y = 30 + random.nextInt(500 - 60);
       addChild(flyingFlag);
-      
+
       juggler.add(flyingFlag);
     }
-  
+
     html.query('#flagCounter').innerHtml = 'Flags: ${numChildren}';
   }
-  
+
   //---------------------------------------------------------------------------------
-  
+
   _removeFlags(int amount) {
-    
+
     while(--amount >= 0 && numChildren > 0) {
       var displayObject = getChildAt(0);
       displayObject.removeFromParent();
       juggler.remove(displayObject);
     }
-  
+
     html.query('#flagCounter').innerHtml = 'Flags: ${numChildren}';
   }
 }

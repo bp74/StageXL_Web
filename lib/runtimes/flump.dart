4abf4c54530db5824fce7dfb1743fd59043aa49d
Bernhard <bernhard.robert.pichler@gmail.com>
2013-07-28 09:59:10 +0200

    updated flump runtime demo

--- lib/runtimes/flump.dart
+++ lib/runtimes/flump.dart
@@ -7,38 +7,29 @@ class FlumpDemo extends DisplayObjectContainer {
     var flumpLibrary = resourceManager.getCustomObject('flump') as FlumpLibrary;
 
     var idle = new FlumpMovie(flumpLibrary, 'idle');
-    idle.x = 500;
-    idle.y = 200;
+    idle.x = 450;
+    idle.y = 150;
     addChild(idle);
 
     var walk = new FlumpMovie(flumpLibrary, 'walk');
     walk.x = 150;
-    walk.y = 460;
+    walk.y = 200;
     addChild(walk);
 
     var attack = new FlumpMovie(flumpLibrary, 'attack');
-    attack.x = 450;
-    attack.y = 460;
+    attack.x = 750;
+    attack.y = 300;
     addChild(attack);
 
     var defeat = new FlumpMovie(flumpLibrary, 'defeat');
-    defeat.x = 720;
-    defeat.y = 460;
+    defeat.x = 350;
+    defeat.y = 400;
     addChild(defeat);
 
     juggler.add(idle);
     juggler.add(walk);
     juggler.add(attack);
     juggler.add(defeat);
-
-    //----------------------------
-
-    BitmapData.load('images/flumpLibraryAtlas0.png').then((bitmapData) {
-      Bitmap bitmap = new Bitmap(bitmapData);
-      bitmap.x = 40;
-      bitmap.y = 40;
-      addChild(bitmap);
-    });
   }
 }
 
